6c9688ad5908c231c8460067e9d3536e2e711c92
royna2544 <whiteshell2544@naver.com>
2025-04-10 11:38:22 +0900

    a30s: Switch to pixel thermal HAL

--- a30s-vendor.mk
+++ a30s-vendor.mk
@@ -40,7 +40,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a30s/proprietary/vendor/lib/hw/camera.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib/hw/gatekeeper.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gatekeeper.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib/hw/sensors.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.exynos7904.so \
-    vendor/samsung/a30s/proprietary/vendor/lib/hw/thermal.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/thermal.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib/libOpenCv.camera.samsung.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOpenCv.camera.samsung.so \
     vendor/samsung/a30s/proprietary/vendor/lib/libSEF.quram.so:$(TARGET_COPY_OUT_VENDOR)/lib/libSEF.quram.so \
     vendor/samsung/a30s/proprietary/vendor/lib/libexynoscamera3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera3.so \
@@ -56,7 +55,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a30s/proprietary/vendor/lib64/hw/camera.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/camera.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib64/hw/gatekeeper.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib64/hw/sensors.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/sensors.exynos7904.so \
-    vendor/samsung/a30s/proprietary/vendor/lib64/hw/thermal.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/thermal.exynos7904.so \
     vendor/samsung/a30s/proprietary/vendor/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so \
     vendor/samsung/a30s/proprietary/vendor/lib64/libOpenCv.camera.samsung.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libOpenCv.camera.samsung.so \
     vendor/samsung/a30s/proprietary/vendor/lib64/libSEF.quram.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libSEF.quram.so \
